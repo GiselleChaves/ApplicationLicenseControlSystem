6e02c81696ecb3505ac6df5f0b1007fd993dd707
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-11-12 11:55:12 -0300

    Criando a classe Catálogo

---
+++ src/Catalogo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalogo {
+  List<Assinaturas> assinaturas;
+  private int cont;
+
+  public Catalogo(){
+    assinaturas = new ArrayList<>();
+    this.cont = 0;
+  }
+
+  public  
+}
