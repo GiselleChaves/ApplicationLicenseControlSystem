12c0dc215ab6c1ee1e2fce4ff8902ac2bd0e678f
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-11-17 18:05:39 -0300

    Classe catalogoAplicativos

---
+++ CatalogoAplicativos.java
@@ -0,0 +1,125 @@
+package aplicativos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import clientes.Cliente;
+
+public class CatalogoAplicativos {
+    List<Aplicativo> listaAplicativos;
+    private int contAplicativos;
+
+    public CatalogoAplicativos(){
+        listaAplicativos = new ArrayList<>();
+        this.contAplicativos = 0;
+    }
+
+    public int getContAplicativos() {
+        return contAplicativos;
+    }
+
+    public void cadastraAppNoCatalogo(Aplicativo app){
+        listaAplicativos.add(app);
+        contAplicativos++;
+    }
+
+    //getby
+    public Aplicativo getAplicativoByCodigo(int codigo) {
+        return listaAplicativos.stream()
+                .filter(aplicativo -> aplicativo.getCodigo() == codigo)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public double getValorMensalByCodigo(int codigo) {
+        Optional<Aplicativo> aplicativoEncontrado = listaAplicativos.stream()
+                .filter(app -> app.getCodigo() == codigo)
+                .findFirst();
+
+        return aplicativoEncontrado.map(Aplicativo::getValorMensalAssinatura).orElse(0.0);
+    }
+
+    //OK, FUNCIONANDO!
+    public void mostraNomeAplicativos(){
+        listaAplicativos
+        .stream()
+        .map(Aplicativo::getNome)
+        .forEach(System.out::println);
+    }            
+
+    public void alterarNomeAplicativo(Predicate<Aplicativo> condicao, Consumer<Aplicativo> oper){
+        for (Aplicativo app : listaAplicativos){
+        if (condicao.test(app)) {
+            oper.accept(app);
+        }
+        }
+    }
+
+    //OK, FUNCIONANDO!
+    public double mostraFaturamentoAndroid() {
+        return listaAplicativos
+                .stream()
+                .filter(item -> item instanceof Aplicativo && "android".equals(((Aplicativo) item).getSo()))
+                .mapToDouble(item -> ((Aplicativo) item).getTotalFaturamentoAndroid())
+                .sum();
+    }
+    //OK, FUNCIONANDO!
+    public double mostraFaturamentoIOS() {
+        return listaAplicativos
+                .stream()
+                .filter(item -> item instanceof Aplicativo && "ios".equals(((Aplicativo) item).getSo()))
+                .mapToDouble(item -> ((Aplicativo) item).getTotalFaturamentoIOS())
+                .sum();
+    }
+
+    //OK
+    public double mostraFaturamentoTotal() {
+        return listaAplicativos
+                .stream()
+                .filter(item -> item instanceof Aplicativo)
+                .mapToDouble(item -> ((Aplicativo) item).getTotalFaturamentoIOS() + ((Aplicativo) item).getTotalFaturamentoAndroid())
+                .sum();
+    }
+
+    public Aplicativo getProdutoNaLinha(int linha) {
+        if (linha >= listaAplicativos.size()) {
+            return null;
+        }
+        return listaAplicativos.get(linha);
+    }
+
+    public int getQtdade() {
+        return listaAplicativos.size();
+    }
+
+    public Stream<Aplicativo> getStream() {
+        return listaAplicativos.stream();
+    }
+
+    public void loadFromFile() {
+        Path appsFilePath = Path.of("aplicativos/aplicativos.txt");
+        try (Stream<String> appsStream = Files.lines(appsFilePath)) {
+            appsStream.forEach(str -> listaAplicativos.add(Aplicativo.fromLineFile(str)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveToFile() {
+        Path appsFilePath = Path.of("aplicativos/aplicativos.txt");
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(appsFilePath, StandardCharsets.UTF_8))) {
+            for (Aplicativo app : listaAplicativos) {
+                writer.println(app.toLineFile());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
